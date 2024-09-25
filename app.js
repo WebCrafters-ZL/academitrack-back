db71cb70b46ba70b196deea9b0a1528205a92101
Leonardo Silva <42047532+leoesilva@users.noreply.github.com>
2024-09-25 08:50:02 -0300

    fix: express-generator, mongoose
    Projeto re-gerado usando o express-generator
    Adicionados os pacotes mongodb e mongoose

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
-import express from 'express';
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-const app = express();
-const port = 3000;
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
-app.get('/', (req, res) => {
-    res.send('Olá mundo!');
-});
+var app = express();
 
-app.listen(port, () => {
-    console.log(`Servidor escutando em http://localhost:${port}`);
-});
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+module.exports = app;
