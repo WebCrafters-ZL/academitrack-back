d7d521f71a46431b5a1ac4eab28e338c08c48358
AraujoThiago0822 <12986262+AraujoThiago0822@users.noreply.github.com>
2024-09-26 22:19:37 -0300

    modelos usuario, aluno,professor e admin

---
+++ models/usuario.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const UsuarioSchema = new mongoose.Schema({
+    email: { type: String, required: true, unique: true},
+    senha: { type: String, required: true}
+});
+
+const Usuario = mongoose.model('Usuario', UsuarioSchema);
+
+module.exports= Usuario;
