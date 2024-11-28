1453076b4d93f73a1c3f97a0314c6dcdb7318671
Caio Cesar <105387511+caiocesardev@users.noreply.github.com>
2024-11-28 05:53:52 -0300

    .

--- controllers/mensagem.controller.js
+++ controllers/mensagem.controller.js
@@ -1,45 +1,72 @@
-const Mensagem = require('../models/mensagem.model');
-const Turma = require('../models/turma.model');
+const {
+  obterUsuarioIdDoToken,
+} = require("../helpers/obterUsuarioIdDoToken.helper");
+const Mensagem = require("../models/mensagem.model");
+const Turma = require("../models/turma.model");
 
 // **Contexto 1: Mensagens para todas as turmas**
 
 const enviarMensagemParaTodasTurmas = async (req, res) => {
   try {
-    const { remetente, assunto, conteudo } = req.body;
+    const token = req.headers.authorization.split(" ")[1]; // Obtém o token do cabeçalho
+    const remetente_id = obterUsuarioIdDoToken(token); // Obtém o remententeID do token
+    const { assunto, conteudo } = req.body;
 
-    if (!remetente || !assunto || !conteudo) {
-      return res.status(400).json({ mensagem: 'Todos os campos obrigatórios devem ser preenchidos' });
+    if ( !assunto || !conteudo) {
+      return res
+        .status(400)
+        .json({
+          mensagem: "Todos os campos obrigatórios devem ser preenchidos",
+        });
     }
 
     // Recupera todas as turmas cadastradas
     const turmas = await Turma.find();
-    const destinatarios = turmas.map(turma => turma._id);
+    const destinatarios = turmas.map((turma) => turma._id);
 
     const novaMensagem = new Mensagem({
-      remetente_id: remetente,
+      remetente_id,
       destinatario_id: destinatarios,
       assunto: assunto,
-      conteudo: conteudo
+      conteudo: conteudo,
     });
 
     await novaMensagem.save();
-    res.status(201).json({ mensagem: 'Mensagem enviada com sucesso para todas as turmas', mensagem: novaMensagem });
+    res
+      .status(201)
+      .json({
+        mensagem: "Mensagem enviada com sucesso para todas as turmas",
+        mensagem: novaMensagem,
+      });
   } catch (erro) {
     console.error(erro);
-    res.status(500).json({ mensagem: 'Erro ao enviar mensagem para todas as turmas', erro: erro.message });
+    res
+      .status(500)
+      .json({
+        mensagem: "Erro ao enviar mensagem para todas as turmas",
+        erro: erro.message,
+      });
   }
 };
 
 const listarMensagensParaTodasTurmas = async (req, res) => {
   try {
     const mensagens = await Mensagem.find()
-      .populate('remetente_id', 'nomeCompleto')
-      .exec();
-    
-    res.status(200).json(mensagens);
+      .populate("remetente_id", "nomeCompleto")
+
+      const mensagensComRemetente = mensagens.map(mensagem => ({
+        remetente: mensagem.remetente_id.nomeCompleto,
+        assunto: mensagem.assunto,
+        conteudo: mensagem.conteudo,
+        dataEnvio: mensagem.dataEnvio
+      }))
+
+    res.status(200).json(mensagensComRemetente);
   } catch (erro) {
-    console.error('Erro ao listar mensagens:', erro);
-    res.status(500).json({ mensagem: 'Erro ao listar mensagens', erro: erro.message });
+    console.error("Erro ao listar mensagens:", erro);
+    res
+      .status(500)
+      .json({ mensagem: "Erro ao listar mensagens", erro: erro.message });
   }
 };
 
@@ -49,14 +76,18 @@ const excluirMensagemDeTodasTurmas = async (req, res) => {
 
     const mensagem = await Mensagem.findById(mensagemId);
     if (!mensagem) {
-      return res.status(404).json({ mensagem: 'Mensagem não encontrada' });
+      return res.status(404).json({ mensagem: "Mensagem não encontrada" });
     }
 
     await Mensagem.findByIdAndDelete(mensagemId);
-    res.status(200).json({ mensagem: 'Mensagem deletada com sucesso de todas as turmas' });
+    res
+      .status(200)
+      .json({ mensagem: "Mensagem deletada com sucesso de todas as turmas" });
   } catch (erro) {
     console.error(erro);
-    res.status(500).json({ mensagem: 'Erro ao deletar mensagem de todas as turmas', erro });
+    res
+      .status(500)
+      .json({ mensagem: "Erro ao deletar mensagem de todas as turmas", erro });
   }
 };
 
@@ -68,26 +99,40 @@ const enviarMensagemParaTurma = async (req, res) => {
     const { turmaId } = req.params;
 
     if (!remetente || !assunto || !conteudo) {
-      return res.status(400).json({ mensagem: 'Todos os campos obrigatórios devem ser preenchidos' });
+      return res
+        .status(400)
+        .json({
+          mensagem: "Todos os campos obrigatórios devem ser preenchidos",
+        });
     }
 
     const turma = await Turma.findById(turmaId);
     if (!turma) {
-      return res.status(404).json({ mensagem: 'Turma não encontrada' });
+      return res.status(404).json({ mensagem: "Turma não encontrada" });
     }
 
     const novaMensagem = new Mensagem({
       remetente_id: remetente,
       destinatario_id: [turmaId],
       assunto: assunto,
-      conteudo: conteudo
+      conteudo: conteudo,
     });
 
     await novaMensagem.save();
-    res.status(201).json({ mensagem: 'Mensagem enviada com sucesso para a turma específica', mensagem: novaMensagem });
+    res
+      .status(201)
+      .json({
+        mensagem: "Mensagem enviada com sucesso para a turma específica",
+        mensagem: novaMensagem,
+      });
   } catch (erro) {
     console.error(erro);
-    res.status(500).json({ mensagem: 'Erro ao enviar mensagem para a turma', erro: erro.message });
+    res
+      .status(500)
+      .json({
+        mensagem: "Erro ao enviar mensagem para a turma",
+        erro: erro.message,
+      });
   }
 };
 
@@ -97,17 +142,22 @@ const listarMensagensPorTurma = async (req, res) => {
 
     const turma = await Turma.findById(turmaId);
     if (!turma) {
-      return res.status(404).json({ mensagem: 'Turma não encontrada' });
+      return res.status(404).json({ mensagem: "Turma não encontrada" });
     }
 
     const mensagens = await Mensagem.find({ destinatario_id: turmaId })
-      .populate('remetente_id', 'nomeCompleto')
+      .populate("remetente_id", "nomeCompleto")
       .exec();
 
     res.status(200).json(mensagens);
   } catch (erro) {
-    console.error('Erro ao listar mensagens:', erro);
-    res.status(500).json({ mensagem: 'Erro ao listar mensagens para a turma', erro: erro.message });
+    console.error("Erro ao listar mensagens:", erro);
+    res
+      .status(500)
+      .json({
+        mensagem: "Erro ao listar mensagens para a turma",
+        erro: erro.message,
+      });
   }
 };
 
@@ -117,23 +167,29 @@ const excluirMensagemDaTurma = async (req, res) => {
 
     const turma = await Turma.findById(turmaId);
     if (!turma) {
-      return res.status(404).json({ mensagem: 'Turma não encontrada' });
+      return res.status(404).json({ mensagem: "Turma não encontrada" });
     }
 
     const mensagem = await Mensagem.findById(mensagemId);
     if (!mensagem) {
-      return res.status(404).json({ mensagem: 'Mensagem não encontrada' });
+      return res.status(404).json({ mensagem: "Mensagem não encontrada" });
     }
 
     if (!mensagem.destinatario_id.includes(turmaId)) {
-      return res.status(400).json({ mensagem: 'Mensagem não pertence a esta turma' });
+      return res
+        .status(400)
+        .json({ mensagem: "Mensagem não pertence a esta turma" });
     }
 
     await Mensagem.findByIdAndDelete(mensagemId);
-    res.status(200).json({ mensagem: 'Mensagem deletada com sucesso da turma específica' });
+    res
+      .status(200)
+      .json({ mensagem: "Mensagem deletada com sucesso da turma específica" });
   } catch (erro) {
     console.error(erro);
-    res.status(500).json({ mensagem: 'Erro ao deletar mensagem da turma', erro });
+    res
+      .status(500)
+      .json({ mensagem: "Erro ao deletar mensagem da turma", erro });
   }
 };
 
@@ -143,5 +199,5 @@ module.exports = {
   excluirMensagemDeTodasTurmas,
   enviarMensagemParaTurma,
   listarMensagensPorTurma,
-  excluirMensagemDaTurma
+  excluirMensagemDaTurma,
 };
--- routes/administrador.routes.js
+++ routes/administrador.routes.js
@@ -1,52 +1,163 @@
-const express = require('express');
-const administradorController = require('../controllers/administrador.controller');
-const alunoController = require('../controllers/aluno.controller');
-const professorController = require('../controllers/professor.controller');
-const cursoController = require('../controllers/curso.controller');
-const disciplinaController = require('../controllers/disciplina.controller');
-const turmaController = require('../controllers/turma.controller');
-const { isLoggedIn, isAdmin } = require('../middlewares/auth.middleware');
+const express = require("express");
+const administradorController = require("../controllers/administrador.controller");
+const alunoController = require("../controllers/aluno.controller");
+const professorController = require("../controllers/professor.controller");
+const cursoController = require("../controllers/curso.controller");
+const disciplinaController = require("../controllers/disciplina.controller");
+const turmaController = require("../controllers/turma.controller");
+const mensagemController = require("../controllers/mensagem.controller");
+const { isLoggedIn, isAdmin } = require("../middlewares/auth.middleware");
 const router = express.Router();
 
 // Rotas para gerenciamento de administradores
-router.get('/perfil', isLoggedIn, isAdmin, administradorController.obterPerfilAdministrador);
-router.put('/perfil', isLoggedIn, isAdmin, administradorController.atualizarPerfilAdministrador);
+router.get(
+  "/perfil",
+  isLoggedIn,
+  isAdmin,
+  administradorController.obterPerfilAdministrador
+);
+router.put(
+  "/perfil",
+  isLoggedIn,
+  isAdmin,
+  administradorController.atualizarPerfilAdministrador
+);
 
 // Rotas para gerenciamento de alunos
-router.get('/alunos/:id', isLoggedIn, isAdmin, alunoController.pesquisarAluno);
-router.get('/alunos', isLoggedIn, isAdmin, alunoController.listarAlunos);
-router.post('/alunos', isLoggedIn, isAdmin, alunoController.cadastrarAluno);
-router.put('/alunos/:id', isLoggedIn, isAdmin, alunoController.atualizarAluno);
-router.delete('/alunos/:id', isLoggedIn, isAdmin, alunoController.deletarAluno);
+router.get("/alunos/:id", isLoggedIn, isAdmin, alunoController.pesquisarAluno);
+router.get("/alunos", isLoggedIn, isAdmin, alunoController.listarAlunos);
+router.post("/alunos", isLoggedIn, isAdmin, alunoController.cadastrarAluno);
+router.put("/alunos/:id", isLoggedIn, isAdmin, alunoController.atualizarAluno);
+router.delete("/alunos/:id", isLoggedIn, isAdmin, alunoController.deletarAluno);
 
 // Rotas para gerenciamento de professores
-router.get('/professores/:id', isLoggedIn, isAdmin, professorController.pesquisarProfessor);
-router.get('/professores', isLoggedIn, isAdmin, professorController.listarProfessores);
-router.post('/professores', isLoggedIn, isAdmin, professorController.cadastrarProfessor);
-router.put('/professores/:id', isLoggedIn, isAdmin, professorController.atualizarProfessor);
-router.delete('/professores/:id', isLoggedIn, isAdmin, professorController.deletarProfessor);
+router.get(
+  "/professores/:id",
+  isLoggedIn,
+  isAdmin,
+  professorController.pesquisarProfessor
+);
+router.get(
+  "/professores",
+  isLoggedIn,
+  isAdmin,
+  professorController.listarProfessores
+);
+router.post(
+  "/professores",
+  isLoggedIn,
+  isAdmin,
+  professorController.cadastrarProfessor
+);
+router.put(
+  "/professores/:id",
+  isLoggedIn,
+  isAdmin,
+  professorController.atualizarProfessor
+);
+router.delete(
+  "/professores/:id",
+  isLoggedIn,
+  isAdmin,
+  professorController.deletarProfessor
+);
 
 // Rotas para gerenciamento de cursos
-router.get('/cursos/:id', isLoggedIn, isAdmin, cursoController.pesquisarCurso);
-router.get('/cursos', isLoggedIn, isAdmin, cursoController.listarCursos);
-router.post('/cursos', isLoggedIn, isAdmin, cursoController.cadastrarCurso);
-router.put('/cursos/:id', isLoggedIn, isAdmin, cursoController.atualizarCurso);
-router.delete('/cursos/:id', isLoggedIn, isAdmin, cursoController.excluirCurso);
+router.get("/cursos/:id", isLoggedIn, isAdmin, cursoController.pesquisarCurso);
+router.get("/cursos", isLoggedIn, isAdmin, cursoController.listarCursos);
+router.post("/cursos", isLoggedIn, isAdmin, cursoController.cadastrarCurso);
+router.put("/cursos/:id", isLoggedIn, isAdmin, cursoController.atualizarCurso);
+router.delete("/cursos/:id", isLoggedIn, isAdmin, cursoController.excluirCurso);
 
 // Rotas para gerenciamento de disciplinas
-router.get('/disciplinas/:id', isLoggedIn, isAdmin, disciplinaController.pesquisarDisciplina);
-router.get('/disciplinas', isLoggedIn, isAdmin, disciplinaController.listarDisciplinas);
-router.post('/disciplinas', isLoggedIn, isAdmin, disciplinaController.cadastrarDisciplina);
-router.put('/disciplinas/:id', isLoggedIn, isAdmin, disciplinaController.atualizarDisciplina);
-router.delete('/disciplinas/:id', isLoggedIn, isAdmin, disciplinaController.excluirDisciplina);
+router.get(
+  "/disciplinas/:id",
+  isLoggedIn,
+  isAdmin,
+  disciplinaController.pesquisarDisciplina
+);
+router.get(
+  "/disciplinas",
+  isLoggedIn,
+  isAdmin,
+  disciplinaController.listarDisciplinas
+);
+router.post(
+  "/disciplinas",
+  isLoggedIn,
+  isAdmin,
+  disciplinaController.cadastrarDisciplina
+);
+router.put(
+  "/disciplinas/:id",
+  isLoggedIn,
+  isAdmin,
+  disciplinaController.atualizarDisciplina
+);
+router.delete(
+  "/disciplinas/:id",
+  isLoggedIn,
+  isAdmin,
+  disciplinaController.excluirDisciplina
+);
 
 // Rotas para gerenciamento de turmas
-router.post('/turmas', isLoggedIn, isAdmin, turmaController.criarTurma);
-router.get('/turmas', isLoggedIn, isAdmin, turmaController.listarTurmas);
-router.get('/turmas/:id', isLoggedIn, isAdmin, turmaController.obterTurma);
-router.put('/turmas/:id', isLoggedIn, isAdmin, turmaController.atualizarTurma);
-router.delete('/turmas/:id', isLoggedIn, isAdmin, turmaController.excluirTurma);
-router.post('/turmas/:id/alunos', isLoggedIn, isAdmin, turmaController.adicionarAluno);
-router.delete('/turmas/:id/alunos', isLoggedIn, isAdmin, turmaController.removerAluno);
+router.post("/turmas", isLoggedIn, isAdmin, turmaController.criarTurma);
+router.get("/turmas", isLoggedIn, isAdmin, turmaController.listarTurmas);
+router.get("/turmas/:id", isLoggedIn, isAdmin, turmaController.obterTurma);
+router.put("/turmas/:id", isLoggedIn, isAdmin, turmaController.atualizarTurma);
+router.delete("/turmas/:id", isLoggedIn, isAdmin, turmaController.excluirTurma);
+router.post(
+  "/turmas/:id/alunos",
+  isLoggedIn,
+  isAdmin,
+  turmaController.adicionarAluno
+);
+router.delete(
+  "/turmas/:id/alunos",
+  isLoggedIn,
+  isAdmin,
+  turmaController.removerAluno
+);
+
+// **Rotas para Mensagens para todas as turmas**
+router.post(
+  "/mensagens/enviar/todas",
+  isLoggedIn,
+  isAdmin,
+  mensagemController.enviarMensagemParaTodasTurmas
+); // Enviar mensagem para todas as turmas
+router.get(
+  "/mensagens/listar/todas",
+  isLoggedIn,
+  isAdmin,
+  mensagemController.listarMensagensParaTodasTurmas
+); // Listar mensagens para todas as turmas
+router.delete(
+  "/mensagens/excluir/todas/:mensagemId",
+  isLoggedIn,
+  isAdmin,
+  mensagemController.excluirMensagemDeTodasTurmas
+); // Excluir mensagem de todas as turmas
+
+// **Rotas para Mensagens para uma unica turma **
+router.post(
+  "/mensagens/enviar/:turmaId",
+  isLoggedIn,
+  isAdmin,
+  mensagemController.enviarMensagemParaTurma
+); // Enviar mensagem para uma unica turma
+router.get(
+  "/mensagens/listar/:turmaId",
+  isLoggedIn,
+  isAdmin,
+  mensagemController.listarMensagensPorTurma
+); // Listar mensagens de uma unica turma
+router.delete(
+  "/mensagens/excluir/:turmaId/:mensagemId",
+  isLoggedIn,
+  isAdmin,
+  mensagemController.excluirMensagemDaTurma
+); // Excluir mensagem de uma unica turma
 
 module.exports = router;
